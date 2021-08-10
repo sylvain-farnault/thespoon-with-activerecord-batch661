e2609fe7a549887f37f6e2c8762133cdf1739a47
Sylvain FARNAULT <sylvain.farnault@gmail.com>
2021-08-10 10:12:24 +0200

    Add 7 crud routes for restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # CRUD FOR Restaurants
+  resources :restaurants, only: [:index, :show]
+  # Create
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
+  # # Read
+  # # All restaurants
+  # get 'restaurants', to: 'restaurants#index'
+  # # One restaurant
+  # get 'restaurants/:id', to: 'restaurants#show'
+
+  # # Update
+  # get 'restaurants/:id/edit', to: 'restaurants#edit'
+  # patch 'restaurants/:id', to: 'restaurants#update'
+  # # Delete
+  # delete 'restaurants/:id', to: 'restaurants#destroy'
+
 end
